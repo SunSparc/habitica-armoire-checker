bc716bde030cb05d7871a1ff33e634d25b7faa2b
Jonathan Duncan <jonathan@nacnud.com>
2022-03-01 17:26:06 -0500

    Adding some tests.

---
+++ request_test.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/smartystreets/assertions/should"
+	"github.com/smartystreets/gunit"
+)
+
+func TestRequestFixture(t *testing.T) {
+	gunit.Run(new(RequestFixture), t)
+}
+
+type RequestFixture struct {
+	*gunit.Fixture
+
+	requester *Requester
+	request   *http.Request
+}
+
+func (this *RequestFixture) Setup() {
+	this.requester = NewRequester()
+	this.request = httptest.NewRequest(http.MethodGet, "/test", nil)
+}
+
+func (this *RequestFixture) TestRequestHasNoError() {
+	err := this.requester.doTheRequest(http.MethodGet, "/")
+	this.So(err, should.BeNil)
+}
+
+// checkArmoire should update this.User and return nil or an error
